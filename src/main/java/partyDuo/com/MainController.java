096cc7b5eef90083c3213f06b36658431247c6bc
kim <kmjun510@naver.com>
2024-10-11 13:44:50 +0900

    DB연결(공통 주석 삭제)

--- src/main/java/partyDuo/com/MainController.java
+++ src/main/java/partyDuo/com/MainController.java
@@ -8,9 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class MainController {
-	@GetMapping({"/","/main"})//웰컴파일없이 제어가능(index.html무시)
+	@GetMapping({"/","/main"})
 	public String home() {
 		log.info("/main");
-		return "main";//resources/templates폴더에서 찾는다
+		return "main";
 	}
 }
