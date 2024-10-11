7fbf7eca730751271a98808221d992f50bbd47eb
kim <kmjun510@naver.com>
2024-10-11 16:55:21 +0900

    오타수정

--- src/main/java/partyDuo/com/service/MemberService.java
+++ src/main/java/partyDuo/com/service/MemberService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
-import partyDuo.com.Mapper.MemberMapper;
+import partyDuo.com.mapper.MemberMapper;
 import partyDuo.com.model.MemberVO;
 
 @Service
