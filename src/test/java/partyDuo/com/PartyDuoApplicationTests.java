dc9d2c3aa80c15136194ae4ff86ea77a5e839e14
Hong Ghee Jung <rlwjd258@gmail.com>
2024-10-11 11:17:33 +0900

    no message

--- src/test/java/partyDuo/com/PartyDuoApplicationTests.java
+++ src/test/java/partyDuo/com/PartyDuoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PartyDuoApplicationTests2 {
+class PartyDuoApplicationTests3 {
 
 	@Test
 	void contextLoads() {
