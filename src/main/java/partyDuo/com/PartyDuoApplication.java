bc85e3af156a3c7ff4913c57cca79a873d5e6cb0
kim <kmjun510@naver.com>
2024-10-11 10:32:34 +0900

    Empty Project

---
+++ src/main/java/partyDuo/com/PartyDuoApplication.java
@@ -0,0 +1,13 @@
+package partyDuo.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyDuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyDuoApplication.class, args);
+	}
+
+}
