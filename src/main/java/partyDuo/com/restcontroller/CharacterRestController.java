d27934006439d01a1f6d9da903eabbc54a47a830
kim <kmjun510@naver.com>
2024-10-21 17:55:32 +0900

    skill, item, hyperstat info

---
+++ src/main/java/partyDuo/com/restcontroller/CharacterRestController.java
@@ -0,0 +1,13 @@
+package partyDuo.com.restcontroller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class CharacterRestController {
+	
+}
